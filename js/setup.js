2430d1ace475c87d683c581b13e93123391eb212
Alice <kuzmina.alisa20@yandex.ru>
2019-09-19 21:04:19 +0300

    Учебный проект, задание "нас орда"

---
+++ js/setup.js
@@ -0,0 +1,54 @@
+'use strict';
+var WIZARD_AMOUNT = 4;
+var WIZARDS_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_CLOTHS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var newWizards = [];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+// Функция, возвращающая случайный элемемент массива
+function getRandomElement(array) {
+  for (var i = 0; i < array.length; i++) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+  }
+  var randomElement = array[randomIndex];
+  return randomElement;
+}
+// Функция, генерирующая массив объектов
+function generateWizard() {
+  for (var i = 0; i < WIZARD_AMOUNT; i++) {
+    newWizards.push({
+      'name': WIZARDS_NAMES[i] + ' ' + WIZARDS_SURNAMES[i],
+      'coatColor': getRandomElement(WIZARD_CLOTHS_COLORS),
+      'eyesColor': getRandomElement(WIZARD_EYES_COLORS)
+    }
+    );
+  }
+  return newWizards;
+}
+generateWizard();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < newWizards.length; i++) {
+  fragment.appendChild(renderWizard(newWizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
---
+++ js/stat.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// константы, в которых записана ширина и высота облака, а так же отступы
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var SHADOW_GAP = 10;
+var GAP = 50;
+var FONT_GAP = 16;
+var BAR_HEIGHT = CLOUD_HEIGHT - GAP - FONT_GAP - GAP;
+var BAR_WIDTH = 40;
+var STAT_Y = CLOUD_HEIGHT - GAP;
+
+// функция отрисовки облака
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+// нахождение максимального элемента в массиве times
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+// рисуем гистограму
+function drawColumn(ctx, names, i) {
+  if (names[i] === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = 'hsl( 240,' + (25 + 70 * Math.random()) + '%, 50%)';
+  }
+}
+window.renderStatistics = function (ctx, names, times) {
+  // цвет тени, смещенной на 10пх вниз и вправо (110 и 60)
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
+  // цвет самого облака
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  // рисуем надпись
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000000';
+  ctx.fillText('Ура, вы победили!', CLOUD_X + 2 * FONT_GAP, CLOUD_Y + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + 2 * FONT_GAP, CLOUD_Y + 2 * FONT_GAP);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    drawColumn(ctx, names, i);
+    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, STAT_Y - FONT_GAP - (BAR_HEIGHT * times[i]) / maxTime + GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, STAT_Y + 2.5 * FONT_GAP);
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, STAT_Y - (BAR_HEIGHT * times[i]) / maxTime);
+  }
+};
